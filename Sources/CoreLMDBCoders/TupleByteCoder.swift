f12838d256af70508bea062be5648a11402936ea
Jaden Geller <jaden.geller@gmail.com>
2023-12-04 12:38:41 -0800

    Add missing throws

--- Sources/CoreLMDBCoders/TupleByteCoder.swift
+++ Sources/CoreLMDBCoders/TupleByteCoder.swift
@@ -29,13 +29,13 @@ extension TupleByteCoder: ByteEncoder where First: ByteEncoder, Second: ByteEnco
 extension TupleByteCoder: PrecountingByteEncoder where First: PrecountingByteEncoder, Second: PrecountingByteEncoder {
     public func withEncoding<Result>(of input: (First.Input, Second.Input), _ body: (UnsafeRawBufferPointer) throws -> Result) throws -> Result {
         var bytes = ContiguousArray<UInt8>()
-        bytes.reserveCapacity(underestimatedByteCount(for: input))
+        bytes.reserveCapacity(try underestimatedByteCount(for: input))
         try first.withEncoding(of: input.0) { bytes.append(contentsOf: $0) }
         try second.withEncoding(of: input.1) { bytes.append(contentsOf: $0) }
         return try bytes.withUnsafeBytes(body)
     }
     
-    public func underestimatedByteCount(for input: (First.Input, Second.Input)) -> Int {
-        first.underestimatedByteCount(for: input.0) + second.underestimatedByteCount(for: input.1)
+    public func underestimatedByteCount(for input: (First.Input, Second.Input)) throws -> Int {
+        try first.underestimatedByteCount(for: input.0) + second.underestimatedByteCount(for: input.1)
     }
 }
